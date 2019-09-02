ea58da46b35d852b43e02de51aa792f560cff8a3
Aman kumar <aman@Amans-MacBook-Air.local>
2019-09-02 20:42:08 +0530

    fixed merge conflicts

--- src/index.js
+++ src/index.js
@@ -2,13 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Controller from '../src/screens/Controller';
 import './index.css';
-import Header from '../src/common/header/Header';
 
 ReactDOM.render(
-<<<<<<< HEAD
-    <Header/>,
-=======
     <Controller/>,
->>>>>>> 60e6a3b5093f9fb0a4c3cf768b7b9325b35aaa63
     document.getElementById('root')
 );
